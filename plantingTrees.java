12b83474f94b310562702e5a72b4b282296a7027
Punit Choudhary <65050160+Punit-Choudhary@users.noreply.github.com>
2021-02-18 19:16:39 +0530

    Create plantingTrees.java

---
+++ plantingTrees.java
@@ -0,0 +1,15 @@
+public class Tree{
+  public static void main(String[] args){
+    // I am gonna introduce myself and print a tree on screen
+    System.out.println("Hey there, I'm Punit and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("Let's plant a tree, to save our Motherland, our Earth");
+    System.out.println();
+    System.out.println("   *     *****     *    ***** ");
+    System.out.println("  ***    *****    ***   ***** ");
+    System.out.println(" *****   *****   *****  ***** ");
+    System.out.println("   *       *       *      *   ");
+    System.out.println("   *       *       *      *   ");
+    System.out.println("   *    #######    *   #######");
+  }
+}
